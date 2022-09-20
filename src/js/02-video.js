7b1d6fda7d8422336eb5ae20b2b563e1b2f9a737
Viktor Annyenkov <annyenkov@gmail.com>
2022-09-20 20:49:32 +0300

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate', throttle(onPlayerTime, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+function onPlayerTime(evt) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(evt))
+        console.log(evt);
+}
+const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+player.setCurrentTime(currentTime.seconds);
