4a689bd13608e4d083c651362882077d8c6195e0
Viktor Annyenkov <annyenkov@gmail.com>
2022-09-23 12:24:19 +0300

    correct task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,19 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
-
+checkCurrentTime();
 player.on('timeupdate', throttle(onPlayerTime, 1000));
+
+function checkCurrentTime() {
+  const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (currentTime) {
+   return player.setCurrentTime(currentTime.seconds);
+  }
+  player.setCurrentTime(0);
+}
 
 function onPlayerTime(evt) {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(evt))
         console.log(evt);
 }
-const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
-player.setCurrentTime(currentTime.seconds);
+
